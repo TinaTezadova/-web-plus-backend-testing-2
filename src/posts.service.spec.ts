588f6907ec8255b406a526711432dbacfd240857
Tina Tezadova <f.tezadova@tinkoff.ru>
2023-04-11 15:48:59 +0300

    add tests

--- src/posts.service.spec.ts
+++ src/posts.service.spec.ts
@@ -9,10 +9,10 @@ describe('PostsService', () => {
 
   describe('.findMany', () => {
     const posts = [
-      {text: 'Post 1'},
-      {text: 'Post 2'},
-      {text: 'Post 3'},
-      {text: 'Post 4'},
+      { text: 'Post 1' },
+      { text: 'Post 2' },
+      { text: 'Post 3' },
+      { text: 'Post 4' },
     ];
 
     beforeEach(() => {
@@ -20,13 +20,32 @@ describe('PostsService', () => {
     });
 
     it('should return all posts if called without options', () => {
-      // реализуйте тест-кейс
+      const findedPosts = postsService.findMany();
+      const mapedPosts = findedPosts.map(item => ({ text: item.text }))
+      expect(mapedPosts).toEqual(expect.arrayContaining(posts));
     });
 
     it('should return correct posts for skip and limit options', () => {
-      // реализуйте тест-кейс
+      const options = { skip: 1, limit: 2 }
+      const findedPosts = postsService.findMany(options);
+      const mapedPosts = findedPosts.map(item => ({ text: item.text }))
+      expect(mapedPosts).toEqual(expect.arrayContaining(posts.slice(options.skip).slice(0, options.limit)));
     });
 
-    // реализуйте недостающие тест-кейсы
+
+    it('should return correct posts for skip option', () => {
+      const options = { skip: 2 }
+      const findedPosts = postsService.findMany(options);
+      const mapedPosts = findedPosts.map(item => ({ text: item.text }))
+      expect(mapedPosts).toEqual(expect.arrayContaining(posts.slice(options.skip)));
+    });
+
+
+    it('should return correct posts for skip option', () => {
+      const options = { limit: 2 }
+      const findedPosts = postsService.findMany(options);
+      const mapedPosts = findedPosts.map(item => ({ text: item.text }))
+      expect(mapedPosts).toEqual(expect.arrayContaining(posts.slice(0, options.limit)));
+    });
   });
 });
